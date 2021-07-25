48e430bd107c2335324729c5ada8a97c6cd60815
Sebastian E. Tassone <tassone_af@yahoo.com>
2021-07-25 18:41:42 -0300

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,12 +8,12 @@ router.get('/:key/:param', async(req, res) => {
 
     try {
 
-        const a = await axios.get(`https://api.yelp.com/v3/businesses/search?categories=parking&term=parking&limit=50&location=${req.params.param}`, {
+        const response = await axios.get(`https://api.yelp.com/v3/businesses/search?categories=parking&term=parking&limit=50&location=${req.params.param}`, {
             'headers': {
                 'Authorization': `Bearer ${req.params.key}`
             }
         })
-        return res.send(a.data)
+        return res.send(response.data)
 
     } catch (error) {
         return res.status(400).send(error)
